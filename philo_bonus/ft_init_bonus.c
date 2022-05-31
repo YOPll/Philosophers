b1f758e68ac74e39c010d8c878633e353a3d9aa8
YOPI 1337 <mrx.ga10@gmail.com>
2022-05-31 18:24:21 +0100

    now creating actions

--- philo_bonus/ft_init_bonus.c
+++ philo_bonus/ft_init_bonus.c
@@ -6,27 +6,41 @@
 /*   By: zyacoubi <zyacoubi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 15:11:55 by zyacoubi          #+#    #+#             */
-/*   Updated: 2022/05/29 18:53:29 by zyacoubi         ###   ########.fr       */
+/*   Updated: 2022/05/31 18:23:56 by zyacoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include_bonus/philo_bonus.h"
 
-void    ft_creat_philos(t_info *philo)
+void    ft_creat_philos(t_info *philo , t_philo *args)
 {
 	int	i;
 	int	id;
-	t_philo	*args;
-
-	args = ft_calloc(philo->nb_philos, sizeof(t_philo));
-	(!args) && ft_exit();
+	
 	i = 0;
 	philo->created_at = ft_get_time();
+	while (i < philo->nb_philos)
+	{
+		id = fork();
+		if (id == 0)
+		{
+			args[i].id = i;
+			//actions here
+		}
+		else
+			philo->table[i] = id;
+	}
+	free(args);
 }
 
 void    ft_init(t_info *philo)
 {
-
+	t_philo	*args;
+	
+	args = ft_calloc(philo->nb_philos, sizeof(t_philo));
+	(!args) && ft_exit();
+	args->philo_info = philo;
+	printf("%lld",args->last_meal);
 	sem_unlink("phi");
 	philo->forks = sem_open("phi", O_CREAT , 0644, philo->nb_philos);
 	if (philo->forks == SEM_FAILED)
@@ -34,5 +48,5 @@ void    ft_init(t_info *philo)
 		ft_puterr("semaphore error");
 		sem_unlink("phi");
 	}
-	ft_creat_philos(philo);
+	//ft_creat_philos(philo);
 }
--- philo_bonus/include_bonus/philo_bonus.h
+++ philo_bonus/include_bonus/philo_bonus.h
@@ -6,7 +6,7 @@
 /*   By: zyacoubi <zyacoubi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 15:12:03 by zyacoubi          #+#    #+#             */
-/*   Updated: 2022/05/29 16:31:51 by zyacoubi         ###   ########.fr       */
+/*   Updated: 2022/05/31 18:22:49 by zyacoubi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ void		check_max_and_min(long x);
 int			ft_exit(void);
 long long	ft_get_time(void);
 long long	ft_current_time(t_philo *philo);
-void		ft_creat_philos(t_info *philo);
+void		ft_creat_philos(t_info *philo, t_philo *args);
 void    	ft_init(t_info *philo);
 
 #endif
